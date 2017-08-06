308fcf4b78b89834197e262c89e725d69fc20d2d
ZeyuWilliamFan <fzy951113@gmail.com>
2017-08-05 23:47:46 -0400

    A python program to deal with SQLite databases. 	- Practice relational DB data model design. 	- Import music track XML files from Itunes export, 	  then parse all data into a new deigned data model.
    Signed-off-by: ZeyuWilliamFan <fzy951113@gmail.com>

--- MusicalTrack.py
+++ MusicalTrack.py
@@ -7,10 +7,16 @@ cur = conn.cursor()
 # Make some fresh tables using executescript()
 cur.executescript('''
 DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Genre;
 DROP TABLE IF EXISTS Album;
 DROP TABLE IF EXISTS Track;
 
 CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name    TEXT UNIQUE
 );
@@ -26,6 +32,7 @@ CREATE TABLE Track (
         AUTOINCREMENT UNIQUE,
     title TEXT  UNIQUE,
     album_id  INTEGER,
+    genre_id INTEGER,
     len INTEGER, rating INTEGER, count INTEGER
 );
 ''')
@@ -46,27 +53,35 @@ def lookup(d, key):
     return None
 
 stuff = ET.parse(fname)
-all = stuff.findall('dict/dict/dict')
-print('Dict count:', len(all))
-for entry in all:
+tracks = stuff.findall('dict/dict/dict')
+print('Dict count:', len(tracks))
+for entry in tracks:
+
     if ( lookup(entry, 'Track ID') is None ) : continue
 
     name = lookup(entry, 'Name')
     artist = lookup(entry, 'Artist')
+    genre = lookup(entry, 'Genre')
     album = lookup(entry, 'Album')
     count = lookup(entry, 'Play Count')
     rating = lookup(entry, 'Rating')
     length = lookup(entry, 'Total Time')
 
-    if name is None or artist is None or album is None :
+    if name is None or artist is None or album is None or genre is None:
         continue
 
-    print(name, artist, album, count, rating, length)
+    print(name, artist, genre, album, count, rating, length)
 
     cur.execute('''INSERT OR IGNORE INTO Artist (name)
         VALUES ( ? )''', ( artist, ) )
     cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
     artist_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Genre (name)
+        VALUES ( ? )''', ( genre, ) )
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]
+
 
     cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id)
         VALUES ( ?, ? )''', ( album, artist_id ) )
@@ -74,8 +89,8 @@ for entry in all:
     album_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count)
-        VALUES ( ?, ?, ?, ?, ? )''',
-        ( name, album_id, length, rating, count ) )
+        (title, album_id, genre_id, len, rating, count)
+        VALUES ( ?, ?, ?, ?, ?, ? )''',
+        ( name, album_id, genre_id, length, rating, count ) )
 
     conn.commit()
